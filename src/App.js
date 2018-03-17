4f34858b6ca2700e434946eb8f26275118f0fd5f
ChenESia <cheneus@gmail.com>
2018-03-16 19:08:40 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import Navbar from './Components/Navbar/navbar.js'
+import './App.css';
+import './index.css'
+
+class App extends Component {
+  render() {
+    return (
+      <Navbar />
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Navbar/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => 
+  <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo right">Logo</a>
+      <ul id="nav-mobile" className="left side-nav fixe">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+
+
+export default Navbar
